7af1d5764004463df5117d5c3cbe08c289add3ae
Walid Elkersh <xxxpascadali@gmail.com>
2026-07-18 09:58:45 +0300

    routine: correcting naming

--- Ringel/CaseCAssembly.lean
+++ Ringel/CaseCAssembly.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Walid K. Elkersh. All rights reserved.
+Copyright (c) 2026 Walid Elkersh. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Walid K. Elkersh
+Authors: Walid Elkersh
 -/
 import Ringel.Primitives
 import Ringel.SmallTree
